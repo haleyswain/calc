09641235fadcb4cae00545edc333d4d9f3c8ccbf
anna lovelace and haley swain <haley@>
2016-09-21 19:04:23 -0700

    if and else statements that allow our calculator to do multiple functions such as add, subtract, multiply, etc.

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,16 @@ $(document).ready(function() {
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
     var operator = $("input:radio[name=operator]:checked").val();
-    console.log("1st number: " + number1);
-    console.log("2nd number: " + number2);
-    console.log("operator: " + operator);
-    var result = add(number1, number2);
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result= subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
